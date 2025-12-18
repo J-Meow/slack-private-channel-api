a7f4a60955f30bc6dc221f68a80941fa142ec405
Jmeow <j@jmeow.net>
2025-12-17 19:32:40 -0600

    Actual API for fetching channel info from id

--- main.ts
+++ main.ts
@@ -4,10 +4,14 @@ const sql = postgres()
 
 const app = new Hono()
 
-app.get("/", async (c) => {
+app.get("/channel/:channel", async (c) => {
     const result =
-        await sql`SELECT id, name, notes, name_confidence FROM slack_channels`
-    return c.json(result)
+        await sql`SELECT id, name, notes, confirmed FROM slack_channels WHERE id=${c.req.param("channel")}`
+    if (result.length) {
+        return c.json({ ...result[0], success: true })
+    } else {
+        return c.json({ id: c.req.param("channel"), success: false })
+    }
 })
 
 Deno.serve(app.fetch)
